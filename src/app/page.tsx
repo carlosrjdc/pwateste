c2a80c55aa07f972011930463f113c5630699a6b
carlos <uppaplicativo@gmail.com>
2024-01-27 13:16:31 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useState } from "react";
+import DrawerDemo from "@/components/info";
 
 export default function Home() {
 	const [scanResult, setScanResult] = useState(null);
@@ -31,6 +32,7 @@ export default function Home() {
 				{scanResult ? <div>Sucess: {scanResult}</div> : <div id="reader"></div>}
 
 				<button className="bg-green-400 p-2 m-1" onClick={()=> setScanResult(null)}>scaner</button>
+				<DrawerDemo/>
 
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Get started by editing&nbsp;
