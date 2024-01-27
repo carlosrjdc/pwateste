9191834f78b00e9ab235cc8ace2aec78b9781b42
carlos <uppaplicativo@gmail.com>
2024-01-27 12:23:05 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,42 @@
 /** @format */
-'use client'
+"use client";
 import Image from "next/image";
-import { useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-	const [qrCode, setQrCode] = useState("");
-	const [data, setData] = useState("No result");
-	const [visible, setVisible] = useState(false);
+	const [scanResult, setScanResult] = useState(null);
 
-	const handleScan = (data: any) => {
-		if (data) {
-			setQrCode(data);
+	useEffect(() => {
+		const scanner = new Html5QrcodeScanner(
+			"reader",
+			{
+				qrbox: {
+					width: 250,
+					height: 250,
+				},
+				fps: 5,
+			},
+			true,
+		);
+
+		scanner.render(success, error);
+
+		function success(result: any) {
+			scanner.clear();
+			setScanResult(result);
 		}
-	};
 
-	const handleError = (err: any) => {
-		console.error(err);
-	};
+		function error(err: any) {
+			console.warn(err);
+		}
+	}, []);
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-
+				
+				{scanResult ? <div>Sucess: {scanResult}</div> : <div id="reader"></div>}
 
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Get started by editing&nbsp;
